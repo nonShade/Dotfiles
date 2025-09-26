433210c7dd1553108e5bc3cac6b14dc4a0af5d09
nonShade <benja.ign23@gmail.com>
2025-09-25 22:37:27 -0300

    feat: update nvim and waybar configs

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,15 +1,16 @@
 return {
-	{
-		"echasnovski/mini.nvim",
-		version = false,
+  {
+    "echasnovski/mini.nvim",
+    version = false,
 
-		config = function()
-			require("mini.align").setup()
-			require("mini.comment").setup()
-			require("mini.trailspace").setup()
-			require("mini.hipatterns").setup()
-			require("mini.icons").setup()
-			MiniIcons.tweak_lsp_kind()
-		end,
-	},
+    config = function()
+      require("mini.align").setup()
+      require("mini.comment").setup()
+      require("mini.trailspace").setup()
+      require("mini.hipatterns").setup()
+      require("mini.icons").setup()
+      require("mini.misc").setup()
+      MiniIcons.tweak_lsp_kind()
+    end,
+  },
 }
