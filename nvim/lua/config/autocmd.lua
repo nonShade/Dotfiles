8d27819ac1b55d0f34ac7b4aa9cf4f4909670824
nonShade <benja.ign23@gmail.com>
2025-11-24 09:38:42 -0300

    feat: add more cmds for nvim

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -30,3 +30,23 @@ vim.api.nvim_create_autocmd("BufRead", {
 		vim.bo.filetype = "dosini"
 	end,
 })
+
+-- Restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function(args)
+		local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+		local line_count = vim.api.nvim_buf_line_count(args.buf)
+		if mark[1] > 0 and mark[1] <= line_count then
+			vim.cmd('normal! g`"zz')
+		end
+	end,
+})
+
+-- removes trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		local save_cursor = vim.fn.getpos(".")
+		vim.cmd([[%s/\s\+$//e]])
+		vim.fn.setpos(".", save_cursor)
+	end,
+})
